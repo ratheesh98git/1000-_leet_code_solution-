a4a72cb0dce00bad825b20663cfd69f02788f3a4
R.Ratheesh <119155481+ratheesh98git@users.noreply.github.com>
2024-08-26 20:17:22 +0530

    day6  22  to 32 program

--- @0_leeet_code _probelem_solution/python_program_33.py
+++ @0_leeet_code _probelem_solution/python_program_33.py
@@ -1,1 +1,13 @@
-`#this is leet code program python_program 	 33.py 
+#33 python program to add two number with out using +operator
+
+
+num1=int(input("eneter the value"))
+num2=10
+
+while num2!=0:
+   c=num1&num2
+   
+   num1=num1^num2
+   
+   num2=c>>1
+print(num1)
